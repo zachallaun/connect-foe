42e1078d1616b7ba69623ecc2fa3c3ec7356c389
Zachary Allaun <zach.allaun@gmail.com>
2013-02-21 20:23:39 -0500

    Represent board as a vector

--- src/connect_foe/engine.clj
+++ src/connect_foe/engine.clj
@@ -1,23 +1,43 @@
 (ns connect-foe.engine)
 
-(defn ->Grid
-  "Constructs an empty 7x6 Connect Four grid, represented by a
-  length-42 vector. Index 0 represents the top left corner of
-  the board."
-  []
-  (vec (repeat 42 nil)))
+(defprotocol IConnectFourGrid
+  (valid-move? [grid move]
+    "`move` is an [x y] pair.")
+  (make-move-no-check [grid move val]
+    "Makes the move without a validity check."))
 
-(defn valid-move?
-  "Given a Connect Four grid and an index, determines whether
-  the move represented by the index is allowed."
-  [board idx]
-  (and
-   ;; the move is in bounds
-   (< -1 idx (count board))
-   ;; the location is empty
-   (not (board idx))
-   ;; the move is on the bottom row
-   (or (<= 35 idx 41)
-       ;; or there is a piece under it
-       (board (+ 7 idx)))))
+(defn make-move
+  "Makes a move if the move is valid; otherwise returns nil."
+  [grid move val]
+  (when (valid-move? grid move)
+    (make-move-no-check grid move val)))
 
+(extend-type clojure.lang.PersistentVector
+  IConnectFourGrid
+  (valid-move? [board [x y]]
+    (let [idx (+ x (* y 7))]
+      (and
+       ;; the move is in bounds
+       (< -1 idx (count board))
+       ;; the location is empty
+       (not (board idx))
+       ;; the move is on the bottom row
+       (or (<= 35 idx 41)
+           ;; or there is a piece under it
+           (board (+ 7 idx))))))
+
+  (make-move-no-check [board [x y] val]
+    (assoc board (+ x (* y 7)) val)))
+
+(defn ->VectorGrid
+  "Constructs a 7x6 Connect Four grid, represented by a length-42
+  vector. Index 0 represents the top left corner of the board.
+
+  init is an optional map of indicies to values used to populate
+  the grid."
+  ([] (->VectorGrid []))
+  ([init]
+     (reduce (fn [grid [idx val]]
+               (assoc grid idx val))
+             (vec (repeat 42 nil))
+             init)))
--- test/connect_foe/engine_test.clj
+++ test/connect_foe/engine_test.clj
@@ -2,20 +2,53 @@
   (:use [midje.sweet]
         [connect-foe.engine]))
 
-(fact "valid moves cannot float"
-      (valid-move? (->Grid) 0)
-      => falsey)
+(defn all [& assertions]
+  (every? identity assertions))
 
-(fact "bottom row is a valid move"
-      (valid-move? (->Grid) 41) => truthy
-      (valid-move? (->Grid) 35) => truthy)
+(defn vector-grid-facts []
+  (all
+   (fact "VectorGrids don't need an init"
+         (->VectorGrid) => vector?)
 
-(fact "on top of another piece is a valid move"
-      (valid-move? (assoc (->Grid) 38 :b) 31) => truthy)
+   (fact "VectorGrids are length 42"
+         (count (->VectorGrid)) => 42)
 
-(fact "same location as another piece is not a valid move"
-      (valid-move? (assoc (->Grid) 38 :b) 38) => falsey)
+   (fact "VectorGrids can be initialized"
+         ((->VectorGrid {0 :foobar}) 0) => :foobar)
 
-(fact "valid moves are within the grid's bounds"
-      (valid-move? (->Grid) -1) => falsey
-      (valid-move? (->Grid) 42) => falsey)
+   (fact "valid moves cannot float"
+         (valid-move? (->VectorGrid) [0 0]) => falsey)
+
+   (fact "bottom row is a valid move"
+         (valid-move? (->VectorGrid) [0 5]) => truthy
+         (valid-move? (->VectorGrid) [6 5]) => truthy)
+
+   (fact "on top of another piece is a valid move"
+         (valid-move? (assoc (->VectorGrid) 38 :b) [3 4]) => truthy)
+
+   (fact "same location as another piece is not a valid move"
+         (valid-move? (assoc (->VectorGrid) 38 :b) [3 5]) => falsey)
+
+   (fact "valid moves are within the grid's bounds"
+         (valid-move? (->VectorGrid) [0 -1]) => falsey
+         (valid-move? (->VectorGrid) [7 5]) => falsey)
+
+   (fact "make-move-no-check will make moves willy nilly"
+         (-> (->VectorGrid)
+             (make-move-no-check [0 0] :move)
+             (nth 0))
+         => :move
+
+         (-> (->VectorGrid {0 :foo})
+             (make-move-no-check [0 0] :bar)
+             (nth 0))
+         => :bar)
+
+   (fact "make-move checks properly"
+         (make-move (->VectorGrid) [0 0] :move) => nil?
+         (make-move (->VectorGrid {41 :foo}) [6 5] :move) => nil?
+
+         (-> (->VectorGrid {41 :foo})
+             (make-move [6 4] :bar)
+             (nth 34))
+         => :bar)))
